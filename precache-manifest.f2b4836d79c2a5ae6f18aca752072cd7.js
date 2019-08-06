968acf396c8e6e40ce7136832a5649157812c8ab
Joshua Aragon <10751127@uvu.edu>
2019-08-06 10:53:09 -0600

    Updates

---
+++ precache-manifest.f2b4836d79c2a5ae6f18aca752072cd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3708e2e42637d17dcc39597419dcf0aa",
+    "url": "/Hustle/index.html"
+  },
+  {
+    "revision": "847ac1bb121a2680da3a",
+    "url": "/Hustle/static/css/main.9a47bb44.chunk.css"
+  },
+  {
+    "revision": "2fcc2699896914d8eb62",
+    "url": "/Hustle/static/js/2.ac79386f.chunk.js"
+  },
+  {
+    "revision": "847ac1bb121a2680da3a",
+    "url": "/Hustle/static/js/main.e70a3a0b.chunk.js"
+  },
+  {
+    "revision": "f4619b1ac0e2ae9e2274",
+    "url": "/Hustle/static/js/runtime~main.80e38428.js"
+  }
+]);
